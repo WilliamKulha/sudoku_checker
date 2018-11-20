02acbbae17a88ff67310e865545e97cda239f6d7
William Kulha <kulha.william@gmail.com>
2018-11-20 17:46:59 -0500

    Add spec to test whether numbers are repeated in any of the three by three grids, returning true if they're not.

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -1,8 +1,9 @@
 import { isValidSudoku } from './../src/module.js';
 import { isValidCol } from './../src/module.js';
 import { isValidRow } from './../src/module.js';
+import { isValidGrids } from './../src/module.js';
 
-describe('seppuku', function() {
+describe('Sudoku Checker', function() {
 
   it('should return true if all rows contain the numbers 1-9', function() {
     let rows = [
@@ -78,6 +79,21 @@ describe('seppuku', function() {
       [2, 1, 4, 5, 6, 7, 8, 9, 1]
     ];
     expect(isValidSudoku(badCompleteArray)).toEqual(false);
+  });
+
+  it(`return true if all 3x3 grids contain no repeats of numbers and are valid sudoku solutions`, function() {
+    let exampleSudoku = [
+      [5, 3, 4, 6, 7, 8, 9, 1, 2], 
+      [6, 7, 2, 1, 9, 5, 3, 4, 8],
+      [1, 9, 8, 3, 4, 2, 5, 6, 7],      
+      [8, 5, 9, 7, 6, 1, 4, 2, 3],
+      [4, 2, 6, 8, 5, 3, 7, 9, 1],
+      [7, 1, 3, 9, 2, 4, 8, 5, 6],      
+      [9, 6, 1, 5, 3, 7, 2, 8, 4],
+      [2, 8, 7, 4, 1, 9, 6, 3, 5],
+      [3, 4, 5, 2, 8, 6, 1, 7, 9]
+    ];
+    expect(isValidGrids(exampleSudoku)).toEqual(true);
   });
 
 });
