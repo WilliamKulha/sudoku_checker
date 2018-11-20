9f5e12bc4e95a927519bf2fecd7e18f6b3ac0375
William Kulha <kulha.william@gmail.com>
2018-11-20 15:02:44 -0500

    Add code to implement function to check if an inputted row is valid.

--- src/module.js
+++ src/module.js
@@ -1,3 +1,15 @@
+export const isValidRow = function(arr) {
+  let isHeAGoodBoi = true;
+  arr.forEach(function(row) {
+    for (let i = 1; i < 10; i++) {
+      if(!row.includes(i)) {
+        isHeAGoodBoi = false;
+      }
+    }
+  });  
+  return isHeAGoodBoi;
+};
 
-
-export const tester = (num1, num2) => num1 + num2;
+export const isValidCol = function(arr) {
+  console.log(arr);
+}
