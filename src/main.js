de8bfeab9e8dc9b81c536e02d7e506d67250532a
William Kulha <kulha.william@gmail.com>
2018-11-20 18:35:30 -0500

    Change main.js so that it doesn't try and run the abortion of a script I was trying to get to work before.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
-import { test } from './module.js';
+/* import { isValidSudoku } from './../src/module.js';
+import { isValidCol } from './../src/module.js';
+import { isValidRow } from './../src/module.js';
+import { isValidGrids } from './../src/module.js';
+*/
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from 'jquery';
 
-$(function() {
-  console.log(test);
+$(document).ready(function() {
+  $('#user_input').submit(function(event) {
+    event.preventDefault();
+  });
 });
