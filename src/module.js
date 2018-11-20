63689c5ffd8ad389d62938271fe07ff92ff9d8af
William Kulha <kulha.william@gmail.com>
2018-11-20 17:47:42 -0500

    Add function to check whether any numbers are repeated in any of the sudoku puzzle's 3x3 grids.

--- src/module.js
+++ src/module.js
@@ -27,6 +27,39 @@ export const isValidCol = function(sudokuArray) {
   return isHeAGoodBoi;
 };
 
+export const isValidGrids = completeSudokuArray => {
+  let isHeAGoodBoi = true;
+
+  let firstThree = completeSudokuArray.slice(0, 3);
+
+  let secondThree = completeSudokuArray.slice(3, 6);
+
+  let thirdThree = completeSudokuArray.slice(6, 9);
+
+  const getArrayCol = (arr, n) => arr.map(x => x[n]);
+
+  let boxOne = getArrayCol(firstThree, 0).concat(getArrayCol(firstThree, 1), getArrayCol(firstThree, 2));
+  let boxTwo = getArrayCol(firstThree, 3).concat(getArrayCol(firstThree, 4), getArrayCol(firstThree, 5));
+  let boxThree = getArrayCol(firstThree, 6).concat(getArrayCol(firstThree, 7), getArrayCol(firstThree, 8));
+  let boxFour = getArrayCol(secondThree, 0).concat(getArrayCol(secondThree, 1), getArrayCol(secondThree, 2));
+  let boxFive = getArrayCol(secondThree, 3).concat(getArrayCol(secondThree, 4), getArrayCol(secondThree, 5));
+  let boxSix = getArrayCol(secondThree, 6).concat(getArrayCol(secondThree, 7), getArrayCol(secondThree, 8));
+  let boxSeven = getArrayCol(thirdThree, 0).concat(getArrayCol(thirdThree, 1), getArrayCol(thirdThree, 2));
+  let boxEight = getArrayCol(thirdThree, 3).concat(getArrayCol(thirdThree, 4), getArrayCol(thirdThree, 5));
+  let boxNine = getArrayCol(thirdThree, 6).concat(getArrayCol(thirdThree, 7), getArrayCol(thirdThree, 8));
+
+  let grids = [boxOne, boxTwo, boxThree, boxFour, boxFive, boxSix, boxSeven, boxEight, boxNine];
+
+  grids.forEach(function(grid) {
+    for (let i = 1; i < 10; i++) {
+      if(!grid.includes(i)) {
+        isHeAGoodBoi = false;
+      }
+    }
+  });
+  return isHeAGoodBoi;
+};
+
 export const isValidSudoku = completeSudokuArray => {
   if (isValidCol(completeSudokuArray) && isValidRow(completeSudokuArray) === true) {
     return true;
