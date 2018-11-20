83b89b8893a6e3055e2868e635581370824c7ca2
William Kulha <kulha.william@gmail.com>
2018-11-20 15:02:16 -0500

    Create tests for checking rows and start on test for checking columns.

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -1,9 +1,36 @@
-import {tester} from './../src/module.js'
+import { isValidRow } from './../src/module.js';
+import { isValidCol } from './../src/module.js';
+describe('seppuku', function() {
 
-describe('tester', function() {
-
-  it('should return the sum of two numbers', function() {
-    let sum = tester(2, 2)
-    expect(sum).toEqual(4);
+  it('should return true if all rows contain the numbers 1-9', function() {
+    let rows = [
+      [1, 2, 3, 4, 5, 6, 7, 8, 9],
+      [1,2, 3, 4, 5, 6, 7, 8, 9]
+    ];
+    expect(isValidRow(rows)).toEqual(true);
   });
+
+  it(`should return false if all rows don't contain the numbers 1-9`, function() {
+    let badRows = [
+      [1, 2, 3, 4, 5, 5, 7, 8, 9],
+      [1, 2, 3, 4, 5, 5, 7, 8, 9]
+    ];
+    expect(isValidRow(badRows)).toEqual(false);
+  });
+
+  it(`should return true if all columns contain the numbers 1-9`, function() {
+    let columns = [
+      [1, 2],
+      [2, 1],
+      [3, 3],
+      [4, 4],
+      [5, 5],
+      [6, 6],
+      [7, 7],
+      [8, 8],
+      [9, 9]
+    ];
+    expect(isValidCol(columns)).toEqual(true);
+  });
+
 });
